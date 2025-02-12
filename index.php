1e9bf05ba3a152ec307c4ed6c9594feb23a12975
pizzaguyapprentice <matisew8247@gmail.com>
2025-02-12 15:12:01 +0000

    not last commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+
+    </head>
+    <body>
+        <h1>We did it!!!</h1>
+    </body>
+</html>
+
+<?php
+    echo "It just works - Todd Howard";
+?>
