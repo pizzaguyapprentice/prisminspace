f23054ff3a5ac964729d8e736a2afbd9f933aa51
pizzaguyapprentice <matisew8247@gmail.com>
2025-03-12 15:53:47 +0000

    Added full CRUD functionality

--- src/userPage/userPage.php
+++ src/userPage/userPage.php
@@ -29,23 +29,65 @@
 
 		if(isset($_POST['username']) && isset($_POST['password'])){
 			try{
-				$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+			$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 			$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			$stmt = $conn->prepare("SELECT firstname FROM users WHERE username = :username AND password = :password");
+
+			$stmt = $conn->prepare("SELECT username FROM users WHERE username = :username AND password = :password");
 			$stmt->bindParam(':username', $_POST['username']);
        		$stmt->bindParam(':password', $_POST['password']);
 			$stmt->execute();
 
 			$user = $stmt->fetch(PDO::FETCH_ASSOC);
+
 			if($user){
-				echo "Welcome, ".$user['firstname']." you have logged in!";
+				echo "Welcome, ".$user['username']." you have logged in!";
 			}else{
 				echo "This account doesn't exist, or you have given incorrect credentials";
 			}
+
 			} catch (PDOException $e){
 				echo "Connection failed: ". $e->getMessage();
 			}
 			
+		}
+		else{
+			echo "Womp womp";
+		}
+
+		?>
+
+		<form method=$_POST>
+			<input type="text" name="username" placeholder="Enter the username to delete" required>
+			<button type="submit" name="deleteButton">Delete Account</button>
+		</form>
+		
+
+		<?php 
+		$servername = "localhost";
+		$username = "root";
+		$password = "123";
+		$dbname = "prisminspacedb";
+
+
+		if(isset($_POST['username'])){
+			try{
+			$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+			$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+			$stmt = $conn->prepare("DELETE FROM users WHERE username = :username");
+			$stmt->bindParam(':username', $_POST['username']);
+
+			$stmt->execute();
+
+			if($stmt->rowCount() == 0){
+				echo "User, ".$user['username']."has been deleted";
+			}else{
+				echo "User has not been found";
+			}
+
+			} catch (PDOException $e){
+				echo "Connection failed: ". $e->getMessage();
+			}
 			
 		}
 		else{
@@ -53,12 +95,7 @@
 			echo "Womp womp";
 			
 		}
-
-	?>
-
-
-
-		<input type="button" value="Delete Account">
+		?>
 	</body>
 
 </html>
