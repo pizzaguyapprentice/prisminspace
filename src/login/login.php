01bf6b508a5286168af941190413d0ad9d4e8f7f
pizzaguyapprentice <matisew8247@gmail.com>
2025-04-20 16:05:37 +0100

    updated login and register page

--- src/login/login.php
+++ src/login/login.php
@@ -19,45 +19,48 @@
     ?>
 
 	<!-- Links based on site -->
-	<link rel ="stylesheet" href="../main.css">
-	<link rel="stylesheet" href="login.css">
+	<link rel="stylesheet" href="login.css?v=<?php echo time(); ?>">
 
     <body>
+
+		<?php
+			include("../grained-master/settings.php");
+		?>
 
 		<?php
 			include "../onload/navbar.php";		
 		?>
 
         <div class="main">
-            <div class="empty">
+			<div class="main-background" id="grained">
 
-            </div>
-            <div class="inputholder">
-                
-                <div id="card1" class="card">
-                    <div class="card-border">
-						<form action="" method="POST">
-                    		<br>
-                    		<h2>Login</h2>
-                    		<br>
-                    		<label for="username">Username: </label>
-							<input type="text" id="username" name="username" required>
-							<br><br><br>
-							<label for="password">Password: </label>
-							<input type="password" id="password" name="password" required>
-							<br><br><br>
-							<input type="submit" value="Login">
-							<?php
-								echo "<br>";
-								echo "<br>";
-								echo "<p style='color: red; font-size:0.6rem'>" . $message . "</p>";
-							?>
-						</form>
-						<br><br><br><br><br><br>
-						<p>Don't Have A Login? <a href="../register/register.php">Register</a> Here!</p>
-                    </div>
-                </div>
-            </div>
+				<div class="cardholder">
+					
+					<div class="card">
+						<div class="card-border">
+							<form action="" method="POST">
+								<br>
+								<h2>Login</h2>
+								<br>
+								<label for="username">Username: </label>
+								<input type="text" id="username" name="username" required>
+								<br><br><br>
+								<label for="password">Password: </label>
+								<input type="password" id="password" name="password" required>
+								<br><br><br>
+								<input type="submit" value="Login" class="loginbutton">
+								<?php
+									echo "<br>";
+									echo "<br>";
+									echo "<p style='color: red; font-size:0.6rem'>" . $message . "</p>";
+								?>
+							</form>
+							<br><br><br><br><br><br>
+							<p>Don't Have A Login? <a href="../login/register.php">Register</a> Here!</p>
+						</div>
+					</div>
+				</div>
+			</div>
 
         </div>
 
@@ -66,6 +69,9 @@
 		?>
        
     </body>
+
+	<script src="../grained-master/options.js">
+    </script>
 </html>
 
 <?php
